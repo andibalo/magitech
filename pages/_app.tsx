8f5df25a78f0528598afdfa9ab73f53aa7c896a3
Andi Usman Balo <47916303+andibalo@users.noreply.github.com>
2022-12-03 17:47:40 +0700

    add ga script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,6 +14,18 @@ export default function App({ Component, pageProps }: AppProps) {
         description="Magitech adalah platform pembelajaran Web, Mobile & Backend Development yang diajarkan oleh mentor dengan pengalaman industri"
         {...SEOConfig}
       />
+       <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-ZRE4P815S6"
+        strategy="afterInteractive"
+      ></Script>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-ZRE4P815S6');
+          `}
+      </Script>
       <Component {...pageProps} />
     </ChakraProvider>
   )
